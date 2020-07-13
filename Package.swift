5b4cc3410a39c3b11b03ac4333b58e9f215442fa
Joshua Asbury <theoriginalbit@me.com>
2020-07-13 10:07:42 +1000

    Update Package.swift to specify platform targets in line with pod spec

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
   name: "Swime",
+  platforms: [
+    .iOS(.v9),
+    .macOS(.v10_10)
+  ],
   products: [
     .library(name: "Swime", targets: ["Swime"])
   ],
